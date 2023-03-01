ebdafd677b069222a55f6b6e095866ee26c23305
ZoomXProg <126647761+ZoomXProg@users.noreply.github.com>
2023-03-01 09:50:00 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+admin_id =1953185001
+token = '5706964401:AAGSvXHMqbxKjP2_t6j8viIyQDPzpJJKsPg'  # токен основного бота, который будет продавать траву
+id = '1953185001'  # Твой ид, что-бы бот кидал тебе все, что происходит в боте
+site = 'https://qiwi.me/peacefulb'  # Cсылка оплаты
+channel = "@chudomarketorsomethingdifferent"  # Канал
+op = "@peacefulb"  # Аккаунт оператора
